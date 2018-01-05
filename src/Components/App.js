7b6a38043dd573b557e9cf999b19ee05623abf93
serzmerz <serzmerz@gmail.com>
2018-01-05 15:52:40 +0200

    clear files

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,19 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import './App.css';
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App'>
+        Text
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
